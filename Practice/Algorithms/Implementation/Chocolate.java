6b92dc4a692cfe2a0c376cd0b6abffc241027069
Sameer Khan <sameer_khan@student.uml.edu>
2019-06-09 16:58:57 -0400

    Some more

--- Practice/Algorithms/Implementation/Chocolate.java
+++ Practice/Algorithms/Implementation/Chocolate.java
@@ -1,16 +1,9 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-public class Solution {
+class Solution {
 
     // Complete the birthday function below.
     static int birthday(List<Integer> s, int d, int m) {
@@ -30,7 +23,7 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        // int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Integer::parseInt)
